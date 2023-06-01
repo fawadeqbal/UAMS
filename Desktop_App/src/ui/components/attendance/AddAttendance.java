654549e677c9c1d8f21431ad9792e97e25e8f50b
Fawad Iqbal <fawadeqbal@yahoo.com>
2023-06-01 20:18:58 +0500

    check/uncheck_all button added

--- Desktop_App/src/ui/components/attendance/AddAttendance.java
+++ Desktop_App/src/ui/components/attendance/AddAttendance.java
@@ -1,5 +1,6 @@
-
 package ui.components.attendance;
+
+import org.jdatepicker.JDatePicker;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableCellRenderer;
@@ -8,23 +9,23 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.text.SimpleDateFormat;
-import controller.Student;
+import controller.StudentController;
 
 import java.util.Date;
-
 
 public class AddAttendance extends JPanel {
 
     private JLabel courseLabel;
     private JComboBox<String> courseComboBox;
     private JLabel dateLabel;
-    private JTextField dateField;
+    private JDatePicker datePicker;
     private JLabel timeSlotLabel;
     private JTextField timeSlotField;
     private JLabel remarksLabel;
     private JTextField remarksField;
     private JTable studentTable;
     private JButton addAttendanceButton;
+    private JCheckBox checkAllCheckBox;
 
     public AddAttendance() {
         initializeComponents();
@@ -36,23 +37,20 @@ public class AddAttendance extends JPanel {
         courseLabel = new JLabel("Course:");
         courseComboBox = new JComboBox<>(new String[]{"Math", "Physics", "English"});
         dateLabel = new JLabel("Date:");
-        dateField = new JTextField(10);
+        datePicker = new JDatePicker(new Date());
+
         timeSlotLabel = new JLabel("Time Slot:");
         timeSlotField = new JTextField(10);
         remarksLabel = new JLabel("Remarks:");
         remarksField = new JTextField(20);
 
-        // Set current date and time slot
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        // Set current time slot
         SimpleDateFormat timeFormat = new SimpleDateFormat("HH:mm");
         Date currentDate = new Date();
-        String dateString = dateFormat.format(currentDate);
         String timeString = timeFormat.format(currentDate);
-        dateField.setText(dateString);
         timeSlotField.setText(timeString);
-        
-        
-        studentTable = new JTable(Student.getStudents()) {
+
+        studentTable = new JTable(StudentController.getStudentsByCourse((String) courseComboBox.getSelectedItem())) {
             @Override
             public Class<?> getColumnClass(int column) {
                 if (column == 2) {
@@ -90,13 +88,13 @@ public class AddAttendance extends JPanel {
         gbc.gridx = 1;
         add(courseComboBox, gbc);
 
-        // Add date label and text field
+        // Add date label and date picker
         gbc.gridx = 0;
         gbc.gridy = 1;
         add(dateLabel, gbc);
 
         gbc.gridx = 1;
-        add(dateField, gbc);
+        add(datePicker, gbc);
 
         // Add time slot label and text field
         gbc.gridx = 0;
@@ -114,9 +112,20 @@ public class AddAttendance extends JPanel {
         gbc.gridx = 1;
         add(remarksField, gbc);
 
-        // Add student table
+        // Add check/uncheck all checkbox
+        checkAllCheckBox = new JCheckBox("Check/Uncheck All");
         gbc.gridx = 0;
         gbc.gridy = 4;
+        gbc.gridwidth = 2;
+        gbc.weightx = 1;
+        gbc.weighty = 0;
+        gbc.fill = GridBagConstraints.NONE;
+        gbc.anchor = GridBagConstraints.LINE_END;
+        add(checkAllCheckBox, gbc);
+
+        // Add student table
+        gbc.gridx = 0;
+        gbc.gridy = 5;
         gbc.gridwidth = 2;
         gbc.weightx = 1;
         gbc.weighty = 1;
@@ -126,21 +135,44 @@ public class AddAttendance extends JPanel {
 
         // Add add attendance button
         gbc.gridx = 0;
-        gbc.gridy = 5;
+        gbc.gridy = 6;
         gbc.gridwidth = 2;
         gbc.weighty = 0;
-        gbc.fill = GridBagConstraints.NONE;
+        gbc.fill = GridBagConstraints.CENTER;
+        gbc.anchor = GridBagConstraints.CENTER;
         add(addAttendanceButton, gbc);
     }
 
     private void addListeners() {
+        courseComboBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedCourse = (String) courseComboBox.getSelectedItem();
+                DefaultTableModel newTableModel = StudentController.getStudentsByCourse(selectedCourse);
+                studentTable.setModel(newTableModel);
+            }
+        });
+
+        checkAllCheckBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                boolean isChecked = checkAllCheckBox.isSelected();
+                DefaultTableModel tableModel = (DefaultTableModel) studentTable.getModel();
+                int rowCount = tableModel.getRowCount();
+                for (int i = 0; i < rowCount; i++) {
+                    tableModel.setValueAt(isChecked, i, 2);
+                }
+            }
+        });
+
         addAttendanceButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 String selectedCourse = (String) courseComboBox.getSelectedItem();
-                String date = dateField.getText();
+                Date selectedDate = (Date) datePicker.getModel().getValue();
                 String timeSlot = timeSlotField.getText();
                 String remarks = remarksField.getText();
+                System.out.println(selectedDate.toString());
 
                 // Get attendance information from the table
                 DefaultTableModel tableModel = (DefaultTableModel) studentTable.getModel();
@@ -152,12 +184,10 @@ public class AddAttendance extends JPanel {
                     boolean isPresent = (Boolean) tableModel.getValueAt(i, 2);
 
                     // Perform attendance saving logic here
-
                     System.out.println("Name: " + name + ", RegNo: " + regNo + ", Present: " + isPresent);
                 }
 
                 // Clear input fields
-                dateField.setText("");
                 timeSlotField.setText("");
                 remarksField.setText("");
 
@@ -166,4 +196,21 @@ public class AddAttendance extends JPanel {
             }
         });
     }
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new JFrame("Add Attendance");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+                AddAttendance addAttendancePanel = new AddAttendance();
+                frame.getContentPane().add(addAttendancePanel);
+
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+        });
+    }
+    
 }
