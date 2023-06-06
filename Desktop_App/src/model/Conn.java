fd5585a46305937e542be31346890f56d3097aee
Faizan <faizanswabi95@gmail.com>
2023-06-06 10:22:52 +0500

    getting a new connection

--- Desktop_App/src/model/Conn.java
+++ Desktop_App/src/model/Conn.java
@@ -14,8 +14,8 @@ import java.sql.SQLException;
 
 public class Conn {
     private static final String URL = "jdbc:mysql://localhost:3306/attendance_system";
-    private static final String USERNAME = "root";
-    private static final String PASSWORD = "fawad321";
+    private static final String USERNAME = "faizan";
+    private static final String PASSWORD = "2546";
     
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USERNAME, PASSWORD);
