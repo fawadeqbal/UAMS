a43b4358a3b0ca06d039c00ece57fc83566dd57a
Fawad Iqbal <122690338+fawadeqbal@users.noreply.github.com>
2023-07-05 10:18:43 +0500

    Role added

--- UAS/src/ui/UAS.java
+++ UAS/src/ui/UAS.java
@@ -1,17 +1,16 @@
 package ui;
 
-import com.formdev.flatlaf.FlatIntelliJLaf;
 import ui.panels.LoginUI;
 import javax.swing.*;
 
 public class UAS {
 
     public static void main(String[] args) {
-//        try {
-//            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         LoginUI loginScreen = new LoginUI();
         loginScreen.setVisible(true); 
