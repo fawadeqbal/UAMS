f9099188f3c6d938a78c5c03854c7d2c81a563fe
Fawad Iqbal <122690338+fawadeqbal@users.noreply.github.com>
2023-07-07 20:45:01 +0500

    schema updated

--- mysqldb/attendance_system_schema.sql
+++ mysqldb/attendance_system_schema.sql
@@ -1,10 +1,21 @@
--- MySQL dump 10.13  Distrib 8.0.33
+CREATE DATABASE  IF NOT EXISTS `attendance` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `attendance`;
+-- MySQL dump 10.13  Distrib 8.0.33, for Linux (x86_64)
 --
--- Host: localhost    Database: attendance_system
+-- Host: 127.0.0.1    Database: attendance
 -- ------------------------------------------------------
--- Server version	8.0.33
+-- Server version	8.0.33-0ubuntu0.22.04.2
 
-
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
 -- Table structure for table `attendance`
@@ -84,6 +95,7 @@ CREATE TABLE `faculty` (
 
 LOCK TABLES `faculty` WRITE;
 /*!40000 ALTER TABLE `faculty` DISABLE KEYS */;
+INSERT INTO `faculty` VALUES (1,'ws','ssa',12);
 /*!40000 ALTER TABLE `faculty` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -145,13 +157,13 @@ LOCK TABLES `student` WRITE;
 UNLOCK TABLES;
 
 --
--- Table structure for table `student_has_course`
+-- Table structure for table `studentenroll`
 --
 
-DROP TABLE IF EXISTS `student_has_course`;
+DROP TABLE IF EXISTS `studentenroll`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `student_has_course` (
+CREATE TABLE `studentenroll` (
   `student_sid` varchar(12) NOT NULL,
   `course_cid` int NOT NULL,
   `course_faculty_t_id` int NOT NULL,
@@ -164,13 +176,48 @@ CREATE TABLE `student_has_course` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `student_has_course`
+-- Dumping data for table `studentenroll`
 --
 
-LOCK TABLES `student_has_course` WRITE;
-/*!40000 ALTER TABLE `student_has_course` DISABLE KEYS */;
-/*!40000 ALTER TABLE `student_has_course` ENABLE KEYS */;
+LOCK TABLES `studentenroll` WRITE;
+/*!40000 ALTER TABLE `studentenroll` DISABLE KEYS */;
+/*!40000 ALTER TABLE `studentenroll` ENABLE KEYS */;
 UNLOCK TABLES;
 
+--
+-- Table structure for table `user`
+--
 
--- Dump completed on 2023-06-06 21:43:20
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user` (
+  `username` varchar(16) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `create_time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `role` varchar(45) NOT NULL,
+  PRIMARY KEY (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('basit','basit@yahoo.com','root','2023-07-07 09:54:32','faculty'),('faizan','faizanswabi95@gmail.com','root','2023-06-06 18:17:14','student'),('fawad','fawadeqbal@yahoo.com','root','2023-06-06 18:32:01','admin');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-07-07 20:43:24
